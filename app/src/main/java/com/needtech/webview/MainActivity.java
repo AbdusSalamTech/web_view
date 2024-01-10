15d5b8e601866e896db3abe66d22a1e496892e51
Jumman04 <arsharifuddinjumman04@gmail.com>
2024-01-10 16:44:16 +0600

    Implimented WebView

--- app/src/main/java/com/needtech/webview/MainActivity.java
+++ app/src/main/java/com/needtech/webview/MainActivity.java
@@ -1,14 +1,74 @@
 package com.needtech.webview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+
+                // Get the error code
+                int errorCode;
+                errorCode = error.getErrorCode();
+
+                // Build the error message
+                String errorMessage = "Error: " + errorCode + "\n" + error.getDescription();
+
+                // Show an AlertDialog with the error message and a reload button
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Error loading page")
+                        .setMessage(errorMessage)
+                        .setPositiveButton("Reload", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Reload the WebView
+                                webView.reload();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Dismiss the dialog
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+
+            }
+        });
+
+        webView.loadUrl(getString(R.string.url));
     }
 }
