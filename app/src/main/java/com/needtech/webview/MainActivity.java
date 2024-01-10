ec38445eeec0c42e7ca2c140239c7164fb1a4729
Jumman04 <arsharifuddinjumman04@gmail.com>
2024-01-10 21:59:16 +0600

    Finish all task of WebView.

--- app/src/main/java/com/needtech/webview/MainActivity.java
+++ app/src/main/java/com/needtech/webview/MainActivity.java
@@ -1,71 +1,82 @@
 package com.needtech.webview;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SetJavaScriptEnabled")
+    private int errorCode = -546;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webView);
-
-        webView.getSettings().setJavaScriptEnabled(true);
+        TextView textView = findViewById(R.id.textView);
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                textView.setVisibility(View.GONE);
+                textView.setText("onPageFinished");
             }
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
+                textView.setVisibility(View.VISIBLE);
+                textView.setText("onPageStarted");
+                errorCode = -546;
+
             }
 
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 super.onReceivedError(view, request, error);
 
-                // Get the error code
-                int errorCode;
+                textView.setVisibility(View.VISIBLE);
                 errorCode = error.getErrorCode();
 
                 // Build the error message
                 String errorMessage = "Error: " + errorCode + "\n" + error.getDescription();
+                textView.setText(errorMessage);
 
                 // Show an AlertDialog with the error message and a reload button
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Error loading page")
                         .setMessage(errorMessage)
-                        .setPositiveButton("Reload", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Reload the WebView
-                                webView.reload();
-                            }
+                        .setPositiveButton("Reload", (dialog, which) -> {
+                            // Reload the WebView
+                            webView.reload();
                         })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Dismiss the dialog
-                                dialog.dismiss();
-                            }
+                        .setNegativeButton("Cancel", (dialog, which) -> {
+                            // Dismiss the dialog
+                            dialog.dismiss();
                         })
                         .show();
 
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100 && errorCode != 546)
+                    textView.setVisibility(View.VISIBLE);
+                else textView.setVisibility(View.GONE);
             }
         });
 
